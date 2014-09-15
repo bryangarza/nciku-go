632a1263126c0449685bb2bfaffb79923fa2975d
Bryan Garza <brygarza@gmail.com>
2014-09-14 22:11:02 -0700

    Achieve working prototype, can find ID of 汉子

--- nciku.go
+++ nciku.go
@@ -5,22 +5,40 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
-func charid(c string) {
-	resp, err := http.Get(fmt.Sprintf("http://www.nciku.com/search/all/%v", c))
+func GetPage(c string) []byte {
+	url := fmt.Sprintf("http://www.nciku.com/search/all/%v", c)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-agent", "Mozilla/5.0")
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%v\n", string(body))
 	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return body
+}
+
+func SearchForID(c string) string {
+	search := fmt.Sprintf("(\\d+)\">%v", c)
+	r, err := regexp.Compile(search)
+	if err != nil {
+		log.Fatal(err)
+	}
+	page := GetPage(c)
+	match := r.Find(page)
+	// taking off the last 5 chars, ie ">好 is 5 chars long
+	match = match[:(len(match) - 5)]
+	return string(match)
 }
 
 func main() {
-	charid("好")
+	// GetPage("好")
+	fmt.Println(SearchForID("好"))
 }
