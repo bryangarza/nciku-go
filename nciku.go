916eabdcf59f8f9582901ffa02bbe4126fa7ddcc
Bryan Garza <brygarza@gmail.com>
2014-09-17 12:58:01 -0700

    use goroutines, still getting panics though

--- nciku.go
+++ nciku.go
@@ -6,10 +6,23 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"time"
 )
 
+var mandarin = []string{
+	"我",
+	"叫",
+	"什",
+	"么",
+	"名",
+	"字",
+}
+
+const searchurl string = "http://www.nciku.com/search/all/%v"
+const swfurl string = "http://images.nciku.com/stroke_order/%v.swf"
+
 func GetPage(c string) []byte {
-	url := fmt.Sprintf("http://www.nciku.com/search/all/%v", c)
+	url := fmt.Sprintf(searchurl, c)
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("User-agent", "Mozilla/5.0")
@@ -36,15 +49,37 @@ func SearchForID(c string) string {
 	// taking off the last 5 chars, ie ">好 is 5 chars long
 	// Chinese characters take up 3 bytes
 	match = match[:(len(match) - 5)]
-	return string(match)
+	matchstr := string(match)
+	return matchstr
 }
 
-func StrokeURL(c string) string {
-	URL := fmt.Sprintf("http://images.nciku.com/stroke_order/%v.swf", SearchForID(c))
-	return URL
+func StrokeURL(mandarin []string) []string {
+	ch := make(chan string)
+	urls := make([]string, 1000)
+
+	for _, c := range mandarin {
+		go func(c string) {
+			url := fmt.Sprintf(swfurl, SearchForID(c))
+			ch <- url
+		}(c)
+	}
+
+	for {
+		select {
+		case url := <-ch:
+			fmt.Println(url)
+			urls = append(urls, url)
+			if len(urls) == len(mandarin) {
+				return urls
+			}
+		case <-time.After(50 * time.Millisecond):
+			fmt.Printf(".")
+		}
+	}
+	return urls
 }
 
 func main() {
 	// GetPage("好")
-	fmt.Println(StrokeURL("好"))
+	fmt.Println(StrokeURL(mandarin))
 }
